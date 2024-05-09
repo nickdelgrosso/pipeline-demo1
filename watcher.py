a1c0f951eafeb928284b0b8a8cd03b5a804c6e7a
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2024-05-09 22:48:03 +0200

    added watcher

---
+++ watcher.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import subprocess
+from watchfiles import watch
+
+
+def run_snakemake(snakemake_file):
+    print("Executing Snakemake pipeline...")
+    subprocess.run(["snakemake", "-s", snakemake_file, "--cores", "1"])
+
+for changes in watch('./data/raw'):
+    for change, path in changes:
+        run_snakemake('./Snakefile')
+        
+        
+
+
+    
